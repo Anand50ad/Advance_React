6ab0c10be980e36c7c40ed63acf49cf3c7ecc12f
Anand50ad <anand50ad@gmail.com>
2021-08-11 09:17:55 +0530

    Use State in action

--- src/tutorial/UseStateBasic.js
+++ src/tutorial/UseStateBasic.js
@@ -1,10 +1,17 @@
 import React, {useState} from 'react'
-
+//use state is a named import and we must include curly braces
 function UseStateBasic() {
-    console.log(useState("Neeraj Chopra"));
-    return (
+const [day, setday] = useState("Today is a rainy day");
+const handleClick=()=>{
+    return(
+    setday("This is a sunny day")
+    );
+}  
+return (
         <div>
-            
+           
+           <button type="button" className="btn" onClick={handleClick}>{day}</button> 
+        
             
         </div>
     )
