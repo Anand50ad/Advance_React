e4d5d77e57962e5f95494e5bb35e0ad51c17e44d
Anand50ad <anand50ad@gmail.com>
2021-08-11 15:31:51 +0530

    targetting value enetered inside a from

--- src/tutorial/Forms.js
+++ src/tutorial/Forms.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 
 function Forms() {
-    const [firstName, setFirstName] = useState("");
-    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState(" ");
+    const [email, setEmail] = useState(" ");
     const handleSubmit =(e)=>{
         e.preventDefault(); //this is used to prevent default browser variable
         console.log("hello");
@@ -13,11 +13,11 @@ function Forms() {
             <form className="form" onSubmit={handleSubmit}>
             <div className="form-control">
                 <label htmlFor="firstName">Name:</label>
-                <input type="text" id="firstName" name="firstname"></input>
+                <input type="text" id="firstName" name="firstname" value={firstName} onChange={(e)=>setFirstName(e.target.value)}></input>
             </div>
             <div className="form-control">
                 <label htmlFor="email">Email:</label>
-                <input type="text" id="email" name="email"></input>
+                <input type="text" id="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
             </div>
             <button type="submit">Submit</button>
             </form>
